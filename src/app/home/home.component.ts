49d1c78a293bff3df7d8405fcd9ac24a1b2fdc83
chinhyanglong <email@example.com>
2022-10-12 20:49:22 +0700

    databinding and ngifelsethen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public name = "Huy";
+  public age = 15;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  public resetName(): void{
+    this.name = "";
   }
 
 }
