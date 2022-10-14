ad48a5f2f73cf20f515434c1e8605e76e6bc027a
Khuong Nguyen <chinhyanglong@gmail.com>
2022-10-14 09:13:09 +0700

    add ng for change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,22 @@ export class HomeComponent implements OnInit {
                   {ten: 'Nho', gia: 13, haGia: false},
                   {ten: 'Cam', gia: -15, haGia: true}
                   ];
-
+  public districts : string[] = ['Quan huyen'];
+  public cities = [{city :'Chon thanh pho', district : ['Quan huyen']},
+    {city :'An Giang', district : ['Châu đốc', "Long Xuyen", "Tri Ton"]},
+  {city :'Vung Tau', district : ['Baf Ria', "Vung Tau", "Tri Ton"]}]
   constructor() { }
 
   public ngOnInit(): void {
     this.fruit;
+    console.log(this.cities);
   }
-
+  public changeCity(event: any) : void {
+    const city = event.target.value;
+    // const search = this.cities.filter(data => data.city === city);
+    // if(search && search.length > 0){
+    //   this.districts = search[0].district;
+    // }
+    this.districts = this.cities.find(p=>p.city == city)?.district || [];
+  }
 }
